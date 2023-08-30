4942be718770a0743a2d4642fe874e5a56ff9391
Diogo-vf <diogof-6@hotmail.com>
2023-08-30 22:41:52 +0200

    Create Record game stats

---
+++ DTOs/GameStats.cs
@@ -0,0 +1,11 @@
+﻿using FindTheNumber.Enums;
+
+namespace FindTheNumber.DTOs;
+
+public record GameStats
+{
+    public required int? LastNumber { get; init; }
+    public required int UserTries { get; init; }
+    public required NumberStatus NumberStatus { get; init; }
+    public required GameStatus GameStatus { get; init; }
+};
